5499f2cbe84dcb72ca212b9c190b44822e7e8fdf
JingBh <jingbohao@yeah.net>
2021-06-05 16:25:12 +0800

    Group uploaded images by date.

--- src/libs/oss.ts
+++ src/libs/oss.ts
@@ -1,9 +1,12 @@
 import axios from 'axios'
+import { DateTime } from 'luxon'
 import { v1 as uuid } from 'uuid'
 
 export const ossSave = (file: Blob) => {
   const id = uuid()
+  const now = DateTime.now().setZone('UTC+8')
+  const date = now.toFormat('yyyy-MM-dd')
   // noinspection JSIgnoredPromiseFromCall
-  axios.put(`https://zgk-avatar.oss-cn-beijing.aliyuncs.com/user/${id}.jpg`, file)
+  axios.put(`https://zgk-avatar.oss-cn-beijing.aliyuncs.com/user/${date}/${id}.jpg`, file)
   window._paq.push(['trackEvent', 'Background', 'Upload', id])
 }
